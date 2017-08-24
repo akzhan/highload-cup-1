0a58ad525da7a7c5708162c39a76e04b2e803c4a
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2017-08-24 14:31:58 +0300

    more heating

--- src/heater.cr
+++ src/heater.cr
@@ -3,5 +3,9 @@ require "http/client"
 Fiber.sleep(5)
 
 HTTP::Client.get "http://localhost/users/1"
-HTTP::Client.get "http://localhost/visits/1"
 HTTP::Client.get "http://localhost/locations/1"
+HTTP::Client.get "http://localhost/visits/1"
+
+HTTP::Client.post "http://localhost/users/1", nil, body: "{}"
+HTTP::Client.post "http://localhost/locations/1", nil, body: "{}"
+HTTP::Client.post "http://localhost/visits/1", nil, body: "{}"
